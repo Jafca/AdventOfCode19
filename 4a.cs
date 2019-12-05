a4c8037dcdaafafb5314031bbd8f6ef657db4311
Jafca <Jafca@users.noreply.github.com>
2019-12-05 18:42:23 +0000

    Create 4a.cs

---
+++ 4a.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+
+namespace AoC19
+{
+    class Program
+    {
+        static void Main()
+        {
+            var count = 0;
+            for (int i = 231832; i <= 767346; i++)
+            {
+                var s = i.ToString().ToList();
+                if (i == int.Parse(string.Concat(s.OrderBy(x => x))))
+                {
+                    for (int j = 0; j < s.Count - 1; j++)
+                    {
+                        if (s[j] == s[j + 1])
+                        {
+                            count++;
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
